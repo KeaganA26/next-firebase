8f265a32b96dc3952581bcb0e2fabc6e0377f4f7
KeaganA26 <ancillkeagan@gmail.com>
2023-03-03 15:55:16 +0200

    added swiper

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import 'swiper/swiper.min.css';
+import 'swiper/components/pagination/pagination.min.css';
+import 'swiper/components/navigation/navigation.min.css';
+
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
+
 }

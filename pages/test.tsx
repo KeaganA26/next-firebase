c0f14bfddf44143c5e62a2673f2252b1d3a3a952
KeaganA26 <ancillkeagan@gmail.com>
2023-03-03 15:28:25 +0200

    working / basic layout

--- pages/test.tsx
+++ pages/test.tsx
@@ -4,10 +4,6 @@ import { useRouter } from "next/router";
 import { useState } from "react";
 import { auth } from "./firebase";
 import styles from "@/styles/Test.module.css";
-import Slider from "react-slick";
-
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 
 const Test: NextPage = () => {
   const [loggedIn, setLoggedIn] = useState(false);
@@ -30,55 +26,28 @@ const Test: NextPage = () => {
     setLoading(false);
   });
 
-  const images = [
-    "https://via.placeholder.com/600x400/ff4136/ffffff?text=Image+1",
-    "https://via.placeholder.com/600x400/0074d9/ffffff?text=Image+2",
-    "https://via.placeholder.com/600x400/3d9970/ffffff?text=Image+3",
-    "https://via.placeholder.com/600x400/ff851b/ffffff?text=Image+4",
-    "https://via.placeholder.com/600x400/b10dc9/ffffff?text=Image+5",
-  ];
-
-  const sliderSettings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  };
-
   return (
-    <div className={styles["color-theme"]}>
+    <div className={styles.container}>
       <nav className={styles.nav}>
-        <ul>
-          <li className={router.pathname === "/" ? "active" : ""}>
-            <a href="/">HOME</a>
-          </li>
-          <li className={router.pathname === "/about" ? "active" : ""}>
-            <a href="/about">ABOUT</a>
-          </li>
-          <li className={router.pathname === "/contact" ? "active" : ""}>
-            <a href="/contact">CONTACT</a>
-          </li>
-          <li className={router.pathname === "/blog" ? "active" : ""}>
-            <a href="/blog">BLOG</a>
-          </li>
-        </ul>
+        <div className={styles.banner}>
+          <ul className={styles.navLinks}>
+            <li className={router.pathname === "/" ? "active" : ""}>
+              <a href="/">HOME</a>
+            </li>
+            <li className={router.pathname === "/about" ? "active" : ""}>
+              <a href="/about">ABOUT</a>
+            </li>
+            <li className={router.pathname === "/contact" ? "active" : ""}>
+              <a href="/contact">CONTACT</a>
+            </li>
+            <li className={router.pathname === "/blog" ? "active" : ""}>
+              <a href="/blog">BLOG</a>
+            </li>
+          </ul>
+          <p>Click on the buttons above to navigate</p>
+        </div>
       </nav>
       <main className={styles.main}>
-        <div
-          style={{ backgroundColor: "red", height: 200, width: 200 }}
-          // className={styles.sliderContainer}
-        >
-          <Slider {...sliderSettings}>
-            {images.map((image, index) => {
-              return (
-                <div key={index}>
-                  <img width={200} height={200} src={image} />
-                </div>
-              );
-            })}
-          </Slider>
-        </div>
         <div className={styles.contentContainer}>
           <h1 className={styles.title}>Welcome to my site!</h1>
           <p className={styles.description}>
@@ -99,4 +68,5 @@ const Test: NextPage = () => {
     </div>
   );
 };
+
 export default Test;
